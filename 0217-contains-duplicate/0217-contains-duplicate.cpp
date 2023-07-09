955cd55520c4c5e6a22c4484158e6d18bd7c8935
oxy9en4 <55944172+oxy9en4@users.noreply.github.com>
2023-07-09 12:28:18 +0900

    Time: 116 ms (98.77%), Space: 57.1 MB (99.72%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int k=0, l = 0;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i]==nums[k] || nums[i]==nums[l]) return true;
+            else if(nums[i]<nums[k] && nums[i] > nums[l]){
+                for(int j=0;j<i;j++){
+                    if(nums[j]==nums[i])return true;
+                }
+            }
+            else{
+                if ( nums[i]>nums[k]){k=i;}
+                else if ( nums[i]<nums[l]) {l=i;}
+            }
+        }
+        return false; 
+    }
+};
