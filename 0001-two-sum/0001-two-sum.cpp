a9d9f3e20a94290c46483e789b1a7580b4ec7af9
oxy9en4 <55944172+oxy9en4@users.noreply.github.com>
2023-07-09 16:22:37 +0900

    Time: 13 ms (73.28%), Space: 10.7 MB (44.59%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> umap;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i){
+            int complement = target - nums[i];
+            if (umap.count(complement) == 1) return { umap[complement], i };
+            umap[nums[i]] = i;
+            
+        }
+        return { };
+    }
+};
+
+
+/* lv 1
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
         for (int i = 0; i < nums.size() - 1; ++i) {
             for (int j = i + 1; j < nums.size(); ++j) {
                 if ((nums[i] + nums[j]) == target) return { i, j };
@@ -9,3 +26,4 @@ public:
         return {};
     }
 };
+*/
