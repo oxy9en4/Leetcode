4d2509f874925926e83e0ecf225089a164664760
oxy9en4 <55944172+oxy9en4@users.noreply.github.com>
2023-07-09 14:34:38 +0900

    Time: 17 ms (28.63%), Space: 7.3 MB (80.40%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s==t;
+    }
+};
+
+/*
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int count[26] = {0};
+        for (auto i : s){
+            count[i - 'a']++;
+        }
+        for (auto i : t){
+            count[i - 'a']--;
+        }
+        for (auto i : count){
+            if (i != 0) return false;
+        }
+        return true;
+    }
+};
+*/
+
+/*
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
         unordered_map<char, int> count;
         for (auto i : s){
             ++count[i];
@@ -14,3 +43,4 @@ public:
         return true;
     }
 };
+*/
