722ee624514ac2ba382c94cf05d653bcea3d4bab
oxy9en4 <55944172+oxy9en4@users.noreply.github.com>
2023-07-09 12:45:07 +0900

    Update 0217-contains-duplicate.cpp

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int k=0, l = 0;
+        int k=0, l = 0; // make range of array
         for(int i=1; i<nums.size(); i++){
             if(nums[i]==nums[k] || nums[i]==nums[l]) return true;
-            else if(nums[i]<nums[k] && nums[i] > nums[l]){
+            else if(nums[i]<nums[k] && nums[i] > nums[l]){  // check if the value is out of range
                 for(int j=0;j<i;j++){
                     if(nums[j]==nums[i])return true;
                 }
