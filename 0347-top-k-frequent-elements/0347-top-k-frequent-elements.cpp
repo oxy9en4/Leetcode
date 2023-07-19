ccc95eba7ec9c2e86102635a4bf15e1e619c2765
oxy9en4 <55944172+oxy9en4@users.noreply.github.com>
2023-07-19 18:22:21 +0900

    Time: 16 ms (80.77%), Space: 13.6 MB (82.94%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         vector<pair<int, int>> vp;
-        unordered_map<int, int> uMap;
+        unordered_map <int, int> umap;
         vector<int> ans;
-        
-        for (auto a: nums){
-            uMap[a]++;
+        for (auto a : nums){
+            umap[a]++;
         }
-        for(auto a: uMap){
+        for (auto a : umap){
             vp.push_back({a.second, a.first});
         }
         sort(vp.rbegin(), vp.rend());
         
-        for (int i=0; i<k; ++i){
+        for (int i = 0; i < k; i++){
             ans.push_back(vp[i].second);
         }
         return ans;
